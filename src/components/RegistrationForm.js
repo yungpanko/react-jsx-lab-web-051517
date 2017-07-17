5d6dda9bcc1ce86cda67b807f24baa58c1c0a7e5
yungpanko <jrjohns@mit.edu>
2017-07-17 15:47:39 -0400

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class RegistrationForm extends React.Component {
+  render() {
+    return <form>
+      <input type='text'></input>
+      <input type='password'></input>
+      <button type='submit'>Submit</button>
+    </form>
+  }
+}
---
+++ src/components/Webpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FillerText from './FillerText.js'
+
+export default class Webpage extends React.Component {
+  render() {
+    return <div>
+      <title>The world's coolest webpage</title>
+      <FillerText/>
+      <FillerText/>
+    </div>
+  }
+}
